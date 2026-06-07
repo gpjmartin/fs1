ec3453d9c65a1ce90c099f210b442d80f320fc65
gpjmartin <gpjmartin@gmail.com>
2026-06-07 20:09:03 +0100

    Update sf1000.sh

--- tpch/sf1000.sh
+++ tpch/sf1000.sh
@@ -12,23 +12,23 @@ for i in 'lineitem'
 do
 	echo -- $i
 	echo values \'$i\' ';'
-	echo 'drop table if exists' $PREFIX$SCHEMA.$i';'
-	echo 'create table' $PREFIX$SCHEMA.$i 'with (format ='$FORMAT') as SELECT * from tpch.'$SCHEMA.$i 'with no data;'
+	echo 'drop table if exists' $PREFIX.$SCHEMA.$i';'
+	echo 'create table' $PREFIX.$SCHEMA.$i 'with (format ='$FORMAT') as SELECT * from tpch.'$SCHEMA.$i 'with no data;'
 	for j in '1992' '1993' '1994' '1995' '1996' '1997' '1998'
 	do
 		for ((x = 1 ; x < $ITERATIONS +1 ; x++)); do
 		echo 'select current_timestamp;' 
-		echo insert into $PREFIX$SCHEMA.$i 'SELECT * from tpch.'$SCHEMA.$i where extract'('year from $COLUMN ')' = $j and extract'('$UNIT from $COLUMN')' = $x ';'
+		echo insert into $PREFIX.$SCHEMA.$i 'SELECT * from tpch.'$SCHEMA.$i where extract'('year from $COLUMN ')' = $j and extract'('$UNIT from $COLUMN')' = $x ';'
 		done
 	done
-	echo 'analyze' $PREFIX$SCHEMA'.'$i';'
+	echo 'analyze' $PREFIX.$SCHEMA'.'$i';'
 done
 for k in 'orders' 'customer' 'nation' 'part' 'partsupp' 'region' 'supplier'
 do
 	echo -- $k
 	echo values \'$k\' ';'
-	echo 'drop table if exists' $PREFIX$SCHEMA.$k';'
+	echo 'drop table if exists' $PREFIX.$SCHEMA.$k';'
 	echo 'select current_timestamp;' 
-	echo 'create table' $PREFIX$SCHEMA.$k 'with (format = '$FORMAT') as SELECT * from tpch.'$SCHEMA.$k';'
-	echo 'analyze' $PREFIX$SCHEMA.$k';'
+	echo 'create table' $PREFIX.$SCHEMA.$k 'with (format = '$FORMAT') as SELECT * from tpch.'$SCHEMA.$k';'
+	echo 'analyze' $PREFIX.$SCHEMA.$k';'
 done
